a78e204b0b6a0126e33ea7e4e86e7979faf7ec34
tokodev <pablo@asdloop.com>
2023-04-25 11:46:40 +0200

    update listings IP

--- src/db.ts
+++ src/db.ts
@@ -37,7 +37,7 @@ function formatSellerAddress(sellerAddress: string): string {
 }
 
 export async function getAllListings() {
-  const response = await fetch('http://192.168.0.30:3001/listings');
+  const response = await fetch('http://85.251.40.44:3001/listings');
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
